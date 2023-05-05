cdcd867a0a54b51141e5a4a4367125fe848c18b2
foxster-mp4 <foxster.utube@gmail.com>
2023-05-05 11:12:04 -0700

    Listen for reminder changes

--- Today/ListViewController/ReminderListViewController+Actions.swift
+++ Today/ListViewController/ReminderListViewController+Actions.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 extension ReminderListViewController {
+    @objc func eventStoreChanged(_ notification: NSNotification) {
+        reminderStoreChanged()
+    }
+    
     @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
         guard let id = sender.id else { return }
         completeReminder(withId: id)
--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -116,6 +116,8 @@ extension ReminderListViewController {
             do {
                 try await reminderStore.requestAccess()
                 reminders = try await reminderStore.readAll()
+                NotificationCenter.default
+                    .addObserver(self, selector: #selector(eventStoreChanged(_:)), name: .EKEventStoreChanged, object: nil)
             } catch TodayError.accessDenied, TodayError.accessRestricted {
                 #if DEBUG
                 reminders = Reminder.sampleData
@@ -123,6 +125,13 @@ extension ReminderListViewController {
             } catch {
                 showError(error)
             }
+            updateSnapshot()
+        }
+    }
+    
+    func reminderStoreChanged() {
+        Task {
+            reminders = try await reminderStore.readAll()
             updateSnapshot()
         }
     }
